f2c2aafef1f13898f1d4d8125da37e93d984d306
Cristian <mlcristianv@gmail.com>
2019-05-22 10:41:48 +0300

    add makefile and edit eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,21 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/recommended',
-    '@vue/standard'
-  ],
+  extends: ["plugin:vue/recommended", "@vue/standard"],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/max-attributes-per-line": ["error", { singleline: 7 }],
+    "vue/singleline-html-element-content-newline": [
+      "error",
+      {
+        ignoreWhenNoAttributes: true,
+        ignoreWhenEmpty: true,
+        ignores: ["a", "li", "router-link"]
+      }
+    ]
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   }
-}
+};
