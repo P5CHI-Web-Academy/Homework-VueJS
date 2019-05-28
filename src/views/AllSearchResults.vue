91c3bc3a6c6bec411cc9b3a22b87276039e70cff
Masha Afonina <mashaafonina@yandex.ru>
2019-05-28 17:04:02 +0300

    Add search results styling

--- src/views/AllSearchResults.vue
+++ src/views/AllSearchResults.vue
@@ -8,41 +8,75 @@
     </v-layout>
     <hr>
 
-    <div>
-      <h2>Questions that contain "{{ query }}"</h2>
-      <div>{{ currentQuestionsResult }}</div>
-      <div v-for="result in resultQuestionsList" :key="result.id">
-        <v-flex>
-          <hr>
-          {{ result }}
-        </v-flex>
-      </div>
-    </div>
-    <br>
+    <v-layout row>
+      <v-flex>
+        <v-card>
+          <v-list three-line subheader>
+            <div>{{ currentQuestionsResult }}</div>
+            <div v-if="resultQuestionsList.length > 0">
+              <v-subheader>Questions that contain "{{ query }}"</v-subheader>
+              <div v-for="result in resultQuestionsList" :key="result.id">
+                <v-list-tile>
+                  <v-list-tile-content>
+                    <v-list-tile-title>
+                      <router-link :to="{ name: 'question', params: { id: result.id } }">
+                        <h3>{{ result.title }}</h3>
+                      </router-link>
+                    </v-list-tile-title>
+                    <v-list-tile-sub-title>Question owner: {{ result.owner.display_name }}</v-list-tile-sub-title>
+                    <div>Tags: {{ result.tags }}</div>
+                  </v-list-tile-content>
+                </v-list-tile>
+                <hr>
+              </div>
+            </div>
+          </v-list>
 
-    <div>
-      <h2>Users that contain "{{ query }}"</h2>
-      <div>{{ currentUsersResult }}</div>
-      <div v-for="result in resultUsersList" :key="result.id">
-        <v-flex>
-          <hr>
-          {{ result }}
-        </v-flex>
-      </div>
-    </div>
-    <br>
+          <v-divider />
 
-    <div>
-      <h2>Tags that contain "{{ query }}"</h2>
-      <div>{{ currentTagsResult }}</div>
-      <div v-for="result in resultTagsList" :key="result.id">
-        <v-flex>
-          <hr>
-          {{ result }}
-        </v-flex>
-      </div>
-    </div>
+          <v-list subheader three-line>
+            <div>{{ currentUsersResult }}</div>
+            <div v-if="resultUsersList.length > 0">
+              <v-subheader>Users that contain "{{ query }}"</v-subheader>
+              <div v-for="result in resultUsersList" :key="result.id">
+                <v-list-tile>
+                  <v-list-tile-content>
+                    <v-list-tile-title>
+                      <router-link :to="{ name: 'user', params: { id: result.id } }">
+                        <h3>{{ result.display_name }}</h3>
+                      </router-link>
+                    </v-list-tile-title>
+                    <v-list-tile-sub-title>User location: {{ result.location }}</v-list-tile-sub-title>
+                    <div>Id: {{ result.id }}</div>
+                  </v-list-tile-content>
+                </v-list-tile>
+                <hr>
+              </div>
+            </div>
+          </v-list>
 
+          <v-divider />
+
+          <v-list subheader three-line>
+            <div>{{ currentTagsResult }}</div>
+            <div v-if="resultTagsList.length > 0">
+              <v-subheader>Tags that contain "{{ query }}"</v-subheader>
+              <div v-for="result in resultTagsList" :key="result.id">
+                <v-list-tile>
+                  <v-list-tile-content>
+                    <v-list-tile-title>
+                      <h3>{{ result.name }}</h3>
+                    </v-list-tile-title>
+                    <v-list-tile-sub-title>Tag count: {{ result.count }}</v-list-tile-sub-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+                <hr>
+              </div>
+            </div>
+          </v-list>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
@@ -63,13 +97,13 @@ export default {
       resultUsersList: 'results/getResultUsersList',
       resultTagsList: 'results/getResultTagsList'
     }),
-    currentQuestionsResult() {
+    currentQuestionsResult () {
       this.fetchQuestionsSearchResults(this.query)
     },
-    currentUsersResult() {
+    currentUsersResult () {
       this.fetchUsersSearchResults(this.query)
     },
-    currentTagsResult() {
+    currentTagsResult () {
       this.fetchTagsSearchResults(this.query)
     }
   },
