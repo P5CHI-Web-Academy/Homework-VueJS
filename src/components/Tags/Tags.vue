ae6fa0247a91326d6f05ae6638be79b3d60de88a
Masha Afonina <mashaafonina@yandex.ru>
2019-05-28 20:01:01 +0300

    Add some styling to users and tags views

--- src/components/Tags/Tags.vue
+++ src/components/Tags/Tags.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
-    <div v-for="tag in tagsList" :key="tag.id">
-      <v-flex>{{ tag }}</v-flex>
-      <hr>
-    </div>
+    <v-layout row wrap class="ml-2">
+      <v-flex v-for="tag in tagsList" :key="tag.id" xs3>
+        <v-divider light />
+        <v-card class="my-2" color="transparent" flat>
+          <v-card-title>
+              <v-btn small light depressed color="#b8def2" class="text-lowercase caption indigo--text">
+                {{ tag.name }}
+              </v-btn>
+          </v-card-title>
+          <div>Count: {{ tag.count }}</div>
+          <!-- <v-flex>{{ tag.name }}</v-flex> -->
+        </v-card>
+
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
--- src/components/Users/Users.vue
+++ src/components/Users/Users.vue
@@ -1,30 +1,42 @@
 <template>
   <div>
-    <div v-for="user in userList" :key="user.id">
-      <v-flex>
-        <v-flex d-inline-flex>
-          <v-avatar
-            tile
-            size="50px"
-            color="grey lighten-4"
-          >
-            <img
-              :src="user.profile_image"
-              alt="avatar"
-            >
-          </v-avatar>
-        </v-flex>
-
-        <v-flex d-inline-flex>
-          <div>
-            <router-link :to="{name: 'user', params: {id: user.id}}">{{ user.display_name }}</router-link>
-          </div>
-          <div>{{ user.location }}</div>
-          <div>{{ user.reputation }}</div>
+    <v-layout row wrap class="ml-2">
+      <v-flex v-for="user in userList" :key="user.id" xs3>
+        <v-flex>
+          <v-divider light />
+          <v-card class="mt-3" color="transparent" flat>
+            <v-layout>
+              <v-flex xs3>
+                <v-avatar
+                  tile
+                  size="50px"
+                  color="grey lighten-4"
+                >
+                  <img
+                    :src="user.profile_image"
+                    alt="avatar"
+                  >
+                </v-avatar>
+              </v-flex>
+              <v-flex xs12>
+                <v-card-title>
+                  <div>
+                    <div>
+                      <router-link :to="{ name: 'user', params: { id: user.id } }">
+                        {{ user.display_name }}
+                      </router-link>
+                    </div>
+                    <div>Location: {{ user.location }}</div>
+                    <div>Reputation: {{ user.reputation }}</div>
+                  </div>
+                </v-card-title>
+              </v-flex>
+            </v-layout>
+          </v-card>
         </v-flex>
       </v-flex>
+    </v-layout>
 
-    </div>
   </div>
 </template>
 
