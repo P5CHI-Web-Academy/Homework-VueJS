e75fd9092035acd54225502ee9ed42f85dc7af7a
Masha Afonina <mashaafonina@yandex.ru>
2019-05-29 13:45:55 +0300

    Add some styling to Question view

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -1,11 +1,63 @@
 <template>
   <div>
     <div v-if="Object.keys(currentQuestion).length > 0">
-      Question: {{ id }}
+      <v-layout row wrap>
+        <v-flex xs9>
+          <div class="ml-3">
+            <router-link :to="{ name: 'question', params: { id: currentQuestion.id } }">
+              <h3>{{ currentQuestion.title }}</h3>
+            </router-link>
+          </div>
+        </v-flex>
+
+        <v-flex xs3 class="text-xs-right">
+          <v-btn color="info">Ask Question</v-btn>
+        </v-flex>
+      </v-layout>
       <hr>
-      <div>
-        {{ currentQuestion }}
-      </div>
+
+      <v-layout class="mx-3 my-3">
+          <v-flex>
+            <div>
+              <v-btn flat class="text-lowercase caption stat-btn">
+                {{ currentQuestion.score }}
+                <br>
+                Votes
+              </v-btn>
+            </div>
+            <div v-if="currentQuestion.answer_count > 0">
+              <v-btn flat outline color="teal" class="text-lowercase caption stat-btn">
+                {{ currentQuestion.answer_count }}
+                <br>
+                Answers
+              </v-btn>
+            </div>
+            <div v-else>
+              <v-btn flat class="text-lowercase caption stat-btn">
+                {{ currentQuestion.answer_count }}
+                <br>
+                Answers
+              </v-btn>
+            </div>
+            <div>
+              <v-btn flat class="text-lowercase caption stat-btn">
+                {{ currentQuestion.view_count }}
+                View
+              </v-btn>
+            </div>
+          </v-flex>
+          <v-flex>
+            <div>
+              {{ currentQuestion.body }}"
+            </div>
+            <br>
+            <v-flex v-for="tag in currentQuestion.tags" :key="tag.id" d-inline-flex>
+              <v-btn small light depressed color="#b8def2" class="text-lowercase caption indigo--text">
+                {{ tag }}
+              </v-btn>
+            </v-flex>
+          </v-flex>
+        </v-layout>
     </div>
     <div v-else>
       <h2>Question not found</h2>
