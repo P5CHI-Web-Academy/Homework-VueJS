3229dc6910bfb24319e72f08cb8f0336f032ef7d
Masha Afonina <mashaafonina@yandex.ru>
2019-05-29 13:31:01 +0300

    Add some styling to User view

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,11 +1,51 @@
 <template>
   <div>
     <div v-if="Object.keys(currentUser).length > 0">
-      User: {{ id }}
+      <!-- User: {{ id }} -->
       <hr>
-      <div>
-        {{ currentUser }}
-      </div>
+      <v-layout row wrap class="mt-5">
+        <v-flex xs3>
+          <v-card class="mx-4">
+            <div class="text-xs-center">
+              <v-avatar
+                tile
+                size="150px"
+                color="grey lighten-4"
+                class="mt-4"
+              >
+                <img
+                  :src="currentUser.profile_image"
+                  alt="avatar"
+                >
+              </v-avatar>
+            </div>
+
+            <v-card-title>
+              <div>
+                <span class="headline">{{ currentUser.reputation }}</span>
+                <span class="grey--text text-uppercase ml-2">reputation</span>
+              </div>
+            </v-card-title>
+            <v-card-actions>
+              <v-btn depressed small color="yellow lighten-4" class="p-0">{{ currentUser.badge_counts.gold }}</v-btn>
+              <v-btn depressed small color="blue-grey lighten-4">{{ currentUser.badge_counts.silver }}</v-btn>
+              <v-btn depressed small color="orange lighten-4">{{ currentUser.badge_counts.bronze }}</v-btn>
+            </v-card-actions>
+
+          </v-card>
+        </v-flex>
+
+        <v-flex xs6>
+          <h2>{{ currentUser.display_name}}</h2>
+          <div>{{ currentUser.website_url }}</div>
+          <div>User Id: {{ currentUser.id }}</div>
+        </v-flex>
+
+        <v-flex xs3>
+          <div>{{ currentUser.location }}</div>
+          <div>{{ currentUser.user_type }}</div>
+        </v-flex>
+      </v-layout>
     </div>
     <div v-else>
       <h2>User not found</h2>
@@ -43,3 +83,14 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .v-btn {
+    min-width: 0;
+  }
+
+  .v-btn {
+    padding: 0 20px;
+  }
+</style>
+
